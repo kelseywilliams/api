c6e661f9fb19102ed506c10b7f59e52054c2193a
Kelsey Williams <kelseywilliams1200@gmail.com>
2021-03-26 15:27:47 -0700

    more not fixing what isn't broke

--- handler.php
+++ handler.php
@@ -31,13 +31,7 @@
                     return false;
                 }
                 else{
-                    $stmt = $mysqli->prepare("UPDATE api_keys SET last_op=? WHERE key=?;");
-                    $stmt->bind_param("ss", $time, $key);
-                    $stmt->execute();
-                    $result = $stmt->affected_rows;
-                    $stmt->close();
-                    //$mysqli->close();
-                    echo $time_to_live . " " . $last_op . " " . $time . " " . $result . " ";
+                    $mysqli->query("UPDATE api_keys SET last_op=\"{$time}\" WHERE key=\"{$key}\";");
                     return true;
                 }
             }
